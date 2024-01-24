4f86b7259a4baa14445a643108380277a96b187c
Pratik22 <2022bit044@sggs.ac.in>
2024-01-24 23:19:54 +0530

    Calci.java file created with addition function in it.

---
+++ Calci.java
@@ -0,0 +1,11 @@
+public class Calci{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = add(a,b);
+    }
+
+    public static int add(int a, int b){
+        return a + b;
+    }
+}
