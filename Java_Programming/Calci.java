26a3161ee607912bcdace70da8c24eba0c6cc3b7
Pratik22 <2022bit044@sggs.ac.in>
2024-01-25 00:43:32 +0530

    Added a new funtion to subtract two numbers, also added comments to the code.

--- Java_Programming/Calci.java
+++ Java_Programming/Calci.java
@@ -4,8 +4,13 @@ public class Calci{
         int b = 20;
         int c = add(a,b);
     }
-
+    //function to add two numbers
     public static int add(int a, int b){
         return a + b;
+    }
+
+    //function to subtract two numbers
+    public static int sub(int a, int b){
+        return a - b;
     }
 }
