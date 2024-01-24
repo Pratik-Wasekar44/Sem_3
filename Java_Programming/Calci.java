61ea88e6abb3414806ac43b85395576052af2697
Pratik22 <2022bit044@sggs.ac.in>
2024-01-25 00:50:08 +0530

    Added a funtion to multiply two numbers.

--- Java_Programming/Calci.java
+++ Java_Programming/Calci.java
@@ -6,11 +6,17 @@ public class Calci{
     }
     //function to add two numbers
     public static int add(int a, int b){
-        return a + b;
+        return (a + b);
     }
 
     //function to subtract two numbers
     public static int sub(int a, int b){
-        return a - b;
+        return (a - b);
+    }
+
+
+    // function to multiply two numbers
+    public static int mul(int a, int b){
+        return (a * b);
     }
 }
